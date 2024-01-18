0fe4c93c509f89f9df09756ce084bbeedcab5e10
K L Latha <lathakljigali@gmail.com>
2024-01-18 21:11:36 +0530

    following maximillian React+Typescript Course

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// import Button from "./components/Button";
+// import Container from "./components/Container";
+
+import { useRef } from "react";
+import Button from "./components/Button.tsx";
+import Form, {type FormHandle} from "./components/Form.tsx";
+import Input from "./components/Input";
+
+ function App(){
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data:unknown){
+    const extractedData = data as {name:string; age:string;} ;
+  console.log(extractedData);
+  customForm.current?.clear();
+  }
+
+  return (
+    <main>
+     <Form onSave={handleSave} ref={customForm}>
+      <Input type="text" label="Name" id="name" />
+      <Input type="number" label="Age" id="age"/>
+      <p>
+        <Button>Save</Button>
+      </p>
+
+     </Form>
+
+    </main>
+  );
+}
+export default App;
